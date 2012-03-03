dddcb25123d760d2e43d34d0eda4833ff5df9b81
dakanji <dakanji@gmail.com>
2012-03-03 09:34:51 +0000

    Add explanation notes to main.php

--- main.php
+++ main.php
@@ -1,2 +1,5 @@
 <?php 
-require_once(dirname(__FILE__) . '/index.php');
+// This file stays for legacy reasons only.
+// All we do here now is to load the contents of index.php
+require_once(dirname(__FILE__) . '/index.php'); 
+?>
